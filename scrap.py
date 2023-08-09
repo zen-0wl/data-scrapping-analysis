bcc022203c237e7196753f5f70f7c954adf1bab0
zen-0wl <65055268+zen-0wl@users.noreply.github.com>
2023-08-09 19:07:39 +0800

    update

--- scrap.py
+++ scrap.py
@@ -1,17 +1,34 @@
-import csv
-from urllib.request import urlopen
+import requests
 from bs4 import BeautifulSoup
-html = urlopen('https://en.wikipedia.org/wiki/Comparison_of_text_editors')
-bs = BeautifulSoup(html, 'html.parser')
-table = bs.findAll('table', {'class': 'wikitable'})[0]
-rows = table.findAll('tr')
-csvFile = open('Text-Editor-Data.csv', 'wt+')
-writer = csv.writer(csvFile)
-try:
-    for row in rows:
-        csvRow = []
-        for cell in row.findAll(['td', 'th']):
-            csvRow.append(cell.get_text())
-            writer.writerow(csvRow)
-finally:
-    csvFile.close()
+import csv
+
+# URL of the Wikipedia page
+url = "https://en.wikipedia.org/wiki/List_of_African_countries_by_population"
+
+# Send an HTTP request and get the page content
+response = requests.get(url)
+html_content = response.content
+
+# Parse the HTML using BeautifulSoup
+soup = BeautifulSoup(html_content, "html.parser")
+
+# Find the table section by its ID
+table_section = soup.find("span", {"id": "Table"})
+
+# Find the table within the section
+table = table_section.find_next("table")
+
+# Extract table data into a list of rows and cells
+table_data = []
+for row in table.find_all("tr"):
+    row_data = [cell.get_text(strip=True) for cell in row.find_all(["th", "td"])]
+    table_data.append(row_data)
+
+# Define the CSV filename
+csv_filename = "african_countries_population.csv"
+
+# Save the table data as a CSV file
+with open(csv_filename, "w", newline="", encoding="utf-8") as csvfile:
+    csv_writer = csv.writer(csvfile)
+    for row_data in table_data:
+        csv_writer.writerow(row_data)
